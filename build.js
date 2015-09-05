8a38372c7cdbb68d2d333cab01966e2a12f2b361
Charles Abeling <charlieabeling@gmail.com>
2015-09-05 12:10:56 -0500

    Add in build

---
+++ build.js
@@ -0,0 +1,18 @@
+//Libs
+var COMPILER = require("cfcompile");
+var FS = COMPILER.getFS();
+
+//Build three source
+//cfcompile js build
+COMPILER.compileFromConfigFile(__dirname + "/compile.json", function(data, err) {
+
+    //Log errors
+    if(err) {console.log(err);}
+
+    //Write the build file
+    console.log("\nWriting new file in /build/bowling.min.js");
+    FS.writeFile(__dirname + "/build/cfautoload.min.js", data, function(err) {
+        if(err) {console.log(err);}
+    });
+
+});
